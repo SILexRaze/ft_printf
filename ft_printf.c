2f5bd3623705321a26914e33a166049d65486b8e
SILexRaze <fonne.victor@gmail.com>
2018-11-27 19:51:55 +0100

    ajout d'une fonction de comptage de flag

--- ft_printf.c
+++ ft_printf.c
@@ -6,14 +6,14 @@
 /*   By: vifonne <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/27 15:13:01 by vifonne           #+#    #+#             */
-/*   Updated: 2018/11/27 16:34:27 by vifonne          ###   ########.fr       */
+/*   Updated: 2018/11/27 19:51:17 by vifonne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 
-int		ft_printf(const char * restrict format, ...)
+int		ft_printf(const char *restrict format, ...)
 {
 	t_list	*lst;
 	t_data	*data;
@@ -22,10 +22,12 @@ int		ft_printf(const char * restrict format, ...)
 
 	i = 0;
 	data->fmt = (char *)format;
-	va_start(arg, format);
 	lst = NULL;
+	va_start(arg, format);
 	lst = ft_lst_parse_flags(data, &lst);
 	ft_print_list(&lst);
+	ft_count_flag(data, &lst);
+	printf("%d\n", data->nb_flag);
 	va_end(arg);
 	return (1);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: vifonne <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/27 10:53:53 by vifonne           #+#    #+#             */
-/*   Updated: 2018/11/27 18:57:45 by vifonne          ###   ########.fr       */
+/*   Updated: 2018/11/27 19:49:13 by vifonne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,13 @@ typedef struct		s_data
 {
 	char			**d_flags;
 	char			*fmt;
+	int				nb_flag;
 }					t_data;
 
 t_list	*ft_lst_parse_flags(t_data *data, t_list **begin_list);
 int		ft_printf(const char * restrict format, ...);
 int		ft_isflag(char c);
+void	ft_count_flag(t_data *data, t_list **begin_list);
 void	ft_lst_push_txt(t_data *data, t_list **begin_list, int start, int j);
+void	ft_lst_push_flag(t_data *datam, t_list **begin_list, int start, int j);
 #endif
