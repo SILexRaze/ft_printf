959d048ed0e8c6177e96e34785d0034018269541
Helene WOLFF <hwolff@e1r10p1.42.fr>
2018-11-28 14:44:10 +0100

    .h norme et readme

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # PRINTF @ 42
 ![Alt Text](https://media.giphy.com/media/I2TqO0oJBaNoc/giphy.gif)
+
+PARTIE OBLIGATOIRE
+
+    %c affichera la valeur ASCII d’un nombre
+    %s afficher une string
+    %p affichera ce que contient un pointeur
+
+    %d affichera un entier
+    %i affichera un entier (int)
+    %o affichera une valeur décimale en octal
+    %u affichera un entier naturel (unsigned)
+    %x affichera une valeur décimale en héxadécimal (lettres en minuscules)
+    %X affichera une valeur décimale en héxadécimal (lettres en majuscules)
+        + flag hh, h, l, ll
+
+    %f affichera une valeur décimale à virgule 
+        + flag l et L
+
+    %% affichera le %
+
+    Flags #0-+ et espace
+
+    Taille minimum du champ (?)
+
+    Gestion de la precision (?)
+
+
+
+
+BONUS
+
+    %b affichera une valeur décimale en binaire
+    %r pour afficher une chaine avec des caractères non imprimables
+    %k pour afficher une date à un format ISO quelconque
--- ft_printf.h
+++ ft_printf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vifonne <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: hwolff <hwolff@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/27 10:53:53 by vifonne           #+#    #+#             */
-/*   Updated: 2018/11/28 14:34:26 by vifonne          ###   ########.fr       */
+/*   Updated: 2018/11/28 14:43:42 by hwolff           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,41 @@
 # define FT_PRINTF_H
 # include "libft/libft.h"
 # include <stdarg.h>
+# include <stdio.h>
 
-typedef struct		s_data
+/*
+** FONCTIONS DU MAN 3 STDARG OK POUR LE PROJET
+** void     va_start(va_list ap, last);
+** type     va_arg(va_list ap, type);
+** void     va_copy(va_list dest, va_list src);
+** void     va_end(va_list ap);
+*/
+
+struct			s_flag
 {
-	char			**d_flags;
-	char			*fmt;
-	int				nb_flag;
-}					t_data;
+	int			hash;
+	int			zero;
+	int			minus;
+	int			plus;
+	int			space;
+}				t_flag;
 
-t_list	*ft_lst_parse_flags(t_data *data, t_list **begin_list);
-int		ft_printf(const char * restrict format, ...);
-int		ft_isconv(char c);
-int		ft_is_univ_flag(char c);
-void	ft_count_conv(t_data *data, t_list **begin_list);
-void	ft_lst_push_txt(t_data *data, t_list **begin_list, int start, int j);
-void	ft_lst_push_flag(t_data *data, t_list **begin_list, int start, int j);
-void	ft_main_parsing(t_data *data, t_list **lst);
+typedef struct	s_data
+{
+	char		**d_flags;
+	char		*fmt;
+	int			nb_flag;
+}				t_data;
+
+t_list			*ft_lst_parse_flags(t_data *data, t_list **begin_list);
+int				ft_printf(const char *restrict format, ...);
+int				ft_isconv(char c);
+int				ft_is_univ_flag(char c);
+void			ft_count_conv(t_data *data, t_list **begin_list);
+void			ft_lst_push_txt(t_data *data, t_list **begin_list, int start,
+					int j);
+void			ft_lst_push_flag(t_data *data, t_list **begin_list, int start,
+					int j);
+void			ft_main_parsing(t_data *data, t_list **lst);
+
 #endif
