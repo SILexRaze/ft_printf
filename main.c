6670c3ddde6ffa796907141c64eb1242c9b2a419
Victor FONNE <fonne.victor@gmail.com>
2018-12-04 22:34:13 +0100

    also workign with -, 0 and field width (combined)

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vifonne <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/27 15:16:34 by vifonne           #+#    #+#             */
-/*   Updated: 2018/12/04 21:37:59 by vifonne          ###   ########.fr       */
+/*   Updated: 2018/12/04 21:48:54 by vifonne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int		main(int ac, char **av)
 	c = "salut";
 
 //	ft_printf("%10X\n", 255);
-	ft_printf("%20s\n%10X\n%10x\n", c, 255, 255);
+	ft_printf("%20s\n%010X\n%--10x\n", c, 255, 255);
 //	ft_printf("%010X\n", 255);
 //	ft_printf("%-50p\n", c);
 //	ft_printf("%050p\n", c);
